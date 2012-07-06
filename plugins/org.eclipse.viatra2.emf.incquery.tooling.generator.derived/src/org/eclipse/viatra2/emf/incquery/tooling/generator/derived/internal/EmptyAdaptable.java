6c97967a61f399bdce135ae34254f584472f89b9
Zoltan Ujhelyi <ujhelyiz@mit.bme.hu>
2012-07-06 14:54:42 +0200

    Dummy IAdaptable created #143

---
+++ plugins/org.eclipse.viatra2.emf.incquery.tooling.generator.derived/src/org/eclipse/viatra2/emf/incquery/tooling/generator/derived/internal/EmptyAdaptable.java
@@ -0,0 +1,18 @@
+package org.eclipse.viatra2.emf.incquery.tooling.generator.derived.internal;
+
+import org.eclipse.core.runtime.IAdaptable;
+
+/**
+ * Unused IAdaptable class that might help overcome a build error
+ * @author Zoltan Ujhelyi
+ *
+ */
+public class EmptyAdaptable implements IAdaptable {
+
+	@Override
+	public Object getAdapter(Class adapter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
